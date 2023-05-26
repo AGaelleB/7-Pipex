86325744d32d0bf25161cb4016ab80c72069cc5a
Anne-gaelle BONNEFOY <abonnefo@e2r3p8.clusters.42paris.fr>
2023-05-26 11:46:19 +0200

    pas de leak si erreurs cmd

--- srcs/data.c
+++ srcs/data.c
@@ -6,7 +6,7 @@
 /*   By: abonnefo <abonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:37:03 by abonnefo          #+#    #+#             */
-/*   Updated: 2023/05/26 11:20:18 by abonnefo         ###   ########.fr       */
+/*   Updated: 2023/05/26 11:45:55 by abonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,16 +53,14 @@ char	*ft_check_paths(char **envp, char *args)
 		}
 		i++;
 	}
+	ft_free_tab(temp_path);
 	if (valid_path != NULL)
 	{
 		if (access(valid_path, F_OK | X_OK) == 0)
 			return (valid_path);
 	}
 	else
-	{
-		ft_free_tab(temp_path);
 		perror("Error : invalid path");
-	}
 	return (valid_path);
 }
 
@@ -72,18 +70,24 @@ t_cmd	split_cmd(char *cmd_av, char **envp)
 
 	command.args = ft_split(cmd_av, ' ');
 	command.path = ft_check_paths(envp, *command.args);
-
-	if (command.path == NULL) // free si erreur dans path
-	{
-		ft_free_tab(command.args);
-		exit(0);
-	}
-	
 	return (command);
 }
 
 void	ft_get_argcs(t_data *data, char **av, char **envp)
 {
+
 	data->cmd1 = split_cmd(av[2], envp);
+	if (data->cmd1.path == NULL)
+	{
+		ft_free_tab(data->cmd1.args);
+		exit(0);
+	}
 	data->cmd2 = split_cmd(av[3], envp);
+	if (data->cmd2.path == NULL)
+	{
+		ft_free_tab(data->cmd1.args);
+		ft_free_tab(data->cmd2.args);
+		free(data->cmd1.path);
+		exit(0);
+	}
 }
