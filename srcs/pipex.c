dfd2603c75132dc8d76756a1d847f0c42df2a981
Anne-gaelle BONNEFOY <abonnefo@paul-f3Br9s1.clusters.42paris.fr>
2023-06-14 15:25:31 +0200

    pipex final

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: abonnefo <abonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 10:40:53 by abonnefo          #+#    #+#             */
-/*   Updated: 2023/06/14 14:35:18 by abonnefo         ###   ########.fr       */
+/*   Updated: 2023/06/14 15:25:11 by abonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ void	child_process_one(t_data *data, char **av, char **envp, int fd[2])
 	}
 	dup2(fd[1], 1);
 	close(fd[0]);
-	close(fd[1]);
 	dup2(f1, 0);
+	close(fd[1]);
 	close(f1);
 	if ((data->cmd1.path != NULL)
 		&& (execve(data->cmd1.path, data->cmd1.args, envp) == -1))
@@ -60,11 +60,11 @@ void	child_process_two(t_data *data, char **av, char **envp, int fd[2])
 		perror(av[4]);
 		exit (-1);
 	}
+	dup2(f2, 1);
+	close(f2);
 	dup2(fd[0], 0);
 	close(fd[0]);
 	close(fd[1]);
-	dup2(f2, 1);
-	close(f2);
 	if ((data->cmd2.path != NULL)
 		&& (execve(data->cmd2.path, data->cmd2.args, envp) == -1))
 	{
